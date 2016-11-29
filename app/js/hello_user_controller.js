dcce4937aa5ac03a9ec6240e95510693369e9599
Thomas Van Cleemput <thomas.vancleemput@ae.be>
2016-11-29 20:12:30 +0100

    clean project

--- app/js/hello_user_controller.js
+++
@@ -1,6 +0,0 @@
-angular.module('HelloUserApp', [])
-      .controller('HelloUserController', function($scope) {
-          $scope.NameChange = function () {
-              $scope.greeting = "Hello " + $scope.name;
-          };
-      });
--- app/js/timer/timer.module.js
+++
@@ -1,7 +0,0 @@
-import timerDirective from './timer.directive';
-import timerService from './timer.service';
-
-// Export timerModule
-export default angular.module('timer', [])
-  .service('TimerService', timerService)
-  .directive('timer', timerDirective);
